e301173610fb27d749ea82864d7f33761da9adc4
Matthew <matthew.ejaz@gmail.com>
2021-12-10 13:06:26 +0100

    comment added

--- app/src/Views/Frontend/show.php
+++ app/src/Views/Frontend/show.php
@@ -1,1 +1,19 @@
 <h1><?= $test; ?></h1>
+
+
+<!-- need dates-->
+<h1>Commentaires : </h1>
+<form>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">ajouter Commentaire</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+<hr>
+<?php $arr = array(1, 2, 3, 4,5); ?>
+<?php foreach($arr as $key=>$value): ?>
+        <h2><?= $key; ?><h2>
+        <p>lballablalblbalblablbalbal<p>
+        <br>
+    <?php endforeach; ?>
+
